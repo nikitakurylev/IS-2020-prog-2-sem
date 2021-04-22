386d867bb7a8186fc71616dd01a68609da29ae15
Nikita Kurylev <n.kurilev@gmail.com>
2021-04-22 23:15:50 +0300

    street counter

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -24,6 +24,7 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 int main() {
 
     map<string, map<int, Route>> routes;
+    map<string, int> streets;
 
     xml_document doc;
     xml_parse_result result = doc.load_file("tree.xml");
@@ -35,17 +36,26 @@ int main() {
         string stationRoutes = stationNode.child_value("routes");
         replace(stationRoutes.begin(), stationRoutes.end(), '.', ',');
         vector<string> routeNumbers = split(stationRoutes,',');
+        string stationName = stationNode.child_value("name_stopping");
+        string stationCoords = stationNode.child_value("coordinates");
+        vector<string> coords = split(stationCoords, ',');
         for(int i = 0; i < routeNumbers.size(); i++){
             int number = atoi(routeNumbers[i].c_str());
-            if (routes[vehicleType].count(number) > 0) {
-                routes[vehicleType].insert(pair<int, Station>(number, Route()));
-                routes[vehicleType][number].AddStation(Station(stationNode.child_value("name_stopping"), vehicleType, 0, 0));
-            }
-            else {
-
-            }
+            routes[vehicleType][number].AddStation(Station(stationName, vehicleType, atof(coords[0].c_str()), atof(coords[0].c_str())));
         }
+
+        string stationLocation = stationNode.child_value("location");
+        vector<string> stationStreets = split(stationLocation, ',');
+        for (int i = 0; i < stationStreets.size(); i++)
+            streets[stationStreets[i]]++;
     }
+
+    string maxstreet = "";
+    for (std::map<string, int>::const_iterator it = streets.begin(); it != streets.end(); ++it)
+        if (it->second > streets[maxstreet])
+            maxstreet = it->first;
+
+    out << maxstreet << " " << streets[maxstreet];
 
     out.close();
     return 0;
