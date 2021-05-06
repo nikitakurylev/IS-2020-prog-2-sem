3c2cea9307508675973265a6f4f2b1886525db4b
Nikita Kurylev <n.kurilev@gmail.com>
2021-05-06 12:14:26 +0300

    Update predicate.hpp

--- homework4/predicate.hpp
+++ homework4/predicate.hpp
@@ -1,6 +1,8 @@
-#pragma once
+#ifndef PREDICATE_H
+#define PREDICATE_H
 #include <iterator>
 #include <vector>
 using namespace std;
 
 bool allOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+#endif
