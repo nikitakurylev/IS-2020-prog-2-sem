f1efb664ee205808c92ea87e284e3add7cc5c483
Nikita Kurylev <n.kurilev@gmail.com>
2021-05-06 12:43:40 +0300

    alpha

--- homework4/predicate.cpp
+++ homework4/predicate.cpp
@@ -5,5 +5,88 @@ bool allOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int
 			return false;
 		++begin;
 	}
-	return  true;
+	return true;
+}
+
+bool anyOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int)) {
+	while (begin != end) {
+		if (comp(*begin))
+			return true;
+		++begin;
+	}
+	return false;
+}
+
+bool noneOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int)) {
+	while (begin != end) {
+		if (comp(*begin))
+			return false;
+		++begin;
+	}
+	return true;
+}
+
+bool oneOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int)) {
+	bool result = false;
+	while (begin != end) {
+		if (comp(*begin)) {
+			if (result)
+				return false;
+			else
+				result = true;
+		}
+		++begin;
+	}
+	return result;
+}
+
+bool isSorted(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int, int)) {
+	while (begin + 1 != end) {
+		if (!comp(*begin, *(begin + 1)))
+			return false;
+		++begin;
+	}
+	return true;
+}
+
+bool isPartitioned(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int)) {
+	bool result = false;
+	while (begin + 1 != end) {
+		if (comp(*begin) != comp(*(begin + 1))) {
+			if (result)
+				return false;
+			else
+				result = true;
+		}
+		++begin;
+	}
+	return result;
+}
+
+int findNot(vector<int>::iterator begin, vector<int>::iterator end, int a) {
+	while (begin != end) {
+		if (*begin == a)
+			break;
+		++begin;
+	}
+	return begin;
+}
+
+int findBackward(vector<int>::iterator begin, vector<int>::iterator end, int a) {
+	while (begin != end) {
+		if (*end == a)
+			break;
+		--end;
+	}
+	return end;
+}
+
+bool isPalindrome(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int, int)) {
+	while (begin < end) {
+		if (!comp(*begin, *end))
+			return false;
+		++begin;
+		--end;
+	}
+	return true;
 }
--- homework4/predicate.hpp
+++ homework4/predicate.hpp
@@ -5,4 +5,12 @@
 using namespace std;
 
 bool allOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+bool anyOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+bool noneOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+bool oneOf(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+bool isSorted(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int, int));
+bool isPartitioned(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int));
+int findNot(vector<int>::iterator begin, vector<int>::iterator end, int a);
+int findBackward(vector<int>::iterator begin, vector<int>::iterator end, int a);
+bool isPalindrome(vector<int>::iterator begin, vector<int>::iterator end, bool comp(int, int));
 #endif
